79a0bc6c36b7c0a7ca8765787d55c2adc04f7d08
Paul Salaets <psalaets@gmail.com>
2017-11-19 21:52:42 -0500

    maintain the illusion

--- test.js
+++ test.js
@@ -1,5 +1,5 @@
 const assert = require('assert');
-const evaluate = require('./dist/index');
+const evaluate = require('./dist/index').default;
 
 describe('evaluate()', function() {
   describe('plain string', function() {
